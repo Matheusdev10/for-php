8b8bb293953737f09d798b3cc0ebafc4e6ad1f8c
Matheus Campos <matheus_501@hotmail.com>
2022-06-20 12:49:56 -0300

    estudando for no php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+for($c = 1; $c<=10; $c++ )
+echo "$c ";
+echo "<br/>";
+for($i=10; $i>=1; $i--)
+echo "$i ";
+echo "<br/>";
+for ($t=0; $t<=100; $t+=5)
+echo "$t ";
+echo "<br/>";
+for($z=20; $z>=10; $z-=2)
+echo "$z ";
+
+?>
+    
+</body>
+</html>
